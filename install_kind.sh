618be445a2707d719ce20e6dfdbd4010e8f5a89a
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-10-13 14:51:20 +0200

    add script for deploying the dev stuff in a kind cluster

---
+++ install_kind.sh
@@ -0,0 +1,7 @@
+
+helm upgrade --install -f local-path-provisioner/values-kind.yaml local-path-provisioner ./local-path-provisioner/
+helm upgrade --install -f redis/values-kind.yaml redis ./redis/
+helm upgrade --install -f minio/values-kind.yaml minio ./minio/
+helm upgrade --install -f postgres-operator/values-kind.yaml postgres-operator ./postgres-operator/
+
+helm upgrade --install -f vat-backend/values-kind.yaml vat-backend ./vat-backend/
---
+++ local-path-provisioner/README.md
@@ -0,0 +1,3 @@
+# Local Path Provisioner
+TODO: describe where the templates are coming from
+
