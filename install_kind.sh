15c1ce3dbea5abbc958b0d3386969da08fbe1923
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-10-13 15:00:36 +0200

    fix some stuff for local deployment

--- install_kind.sh
+++ install_kind.sh
@@ -1,5 +1,6 @@
 
 helm upgrade --install -f local-path-provisioner/values-kind.yaml local-path-provisioner ./local-path-provisioner/
+helm upgrade --install -f external-secrets/values.yaml external-secrets ./external-secrets/
 helm upgrade --install -f redis/values-kind.yaml redis ./redis/
 helm upgrade --install -f minio/values-kind.yaml minio ./minio/
 helm upgrade --install -f postgres-operator/values-kind.yaml postgres-operator ./postgres-operator/
