fc4ac025de45fce1326a13c678412b3144bee855
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-10-14 17:42:53 +0200

    fix: bugs to make django work in kind

--- install_kind.sh
+++ install_kind.sh
@@ -1,8 +1,12 @@
 
-helm upgrade --install -f local-path-provisioner/values-kind.yaml local-path-provisioner ./local-path-provisioner/
-helm upgrade --install -f external-secrets/values.yaml external-secrets ./external-secrets/
-helm upgrade --install -f redis/values-kind.yaml redis ./redis/
-helm upgrade --install -f minio/values-kind.yaml minio ./minio/
-helm upgrade --install -f postgres-operator/values-kind.yaml postgres-operator ./postgres-operator/
+kubectl create ns local-path-provisioner 
+kubectl create ns redis
+#kubectl create ns minio
+kubectl create ns postgres-operator
+kubectl create ns vat-backend
+helm upgrade --install --namespace local-path-provisioner -f local-path-provisioner/values-kind.yaml local-path-provisioner ./local-path-provisioner/
+helm upgrade --install --namespace redis -f redis/values-kind.yaml redis ./redis/
+#helm upgrade --install --namespace minio -f minio/values-kind.yaml minio ./minio/
+helm upgrade --install --namespace postgres-operator -f postgres-operator/values-kind.yaml postgres-operator ./postgres-operator/
 
-helm upgrade --install -f vat-backend/values-kind.yaml vat-backend ./vat-backend/
+helm upgrade --install --namespace vat-backend -f vat-backend/values-kind.yaml vat-backend ./vat-backend/
